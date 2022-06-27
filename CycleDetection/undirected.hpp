559cca4dc35d1f8c3d8ddc9b93e643c355508ec3
Akshaj <67319076+Akshaj-Padmakar@users.noreply.github.com>
2022-06-27 12:38:03 +0530

    Create undirected.hpp

---
+++ CycleDetection/undirected.hpp
@@ -0,0 +1,18 @@
+//any edge that connects it to its ancestor which is not its parent is called a "BACK-EDGE"
+//BACK-EDGE indicates a cycle in the graph
+
+function<bool(int, int)> dfs = [&](int node, int p) {
+	vis[node] = 1;
+	for (auto ch : g[node]) {
+		if (vis[ch]) {
+			if (ch != p) {
+				return true;
+			}
+		} else {
+			if (dfs(ch, node)) {
+				return true;
+			}
+		}
+	}
+	return false;
+};
