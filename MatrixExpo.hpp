e8053165241ebaecbe5afa771f55d9f590bec126
Akshaj <67319076+Akshaj-Padmakar@users.noreply.github.com>
2022-06-27 10:53:42 +0530

    Update MatrixExpo.hpp

--- MatrixExpo.hpp
+++ MatrixExpo.hpp
@@ -1,3 +1,4 @@
+//--------------------------------------<INT MATRIX>-------------------------------------------------
 const long long MOD2 = static_cast<long long>(MOD) * MOD;
 
 struct Matrix {
@@ -49,3 +50,59 @@ Matrix pw(Matrix a, int p) {
     }
     return result;
 }
+
+
+
+
+//--------------------------------------<LONG DOUBLE MATRIX>-------------------------------------------------
+
+struct Matrix {
+    vector< vector<long double> > mat;
+    int n_rows, n_cols;
+
+    Matrix() {}
+
+    Matrix(vector< vector<long double> > values): mat(values), n_rows(values.size()),
+        n_cols(values[0].size()) {}
+
+    static Matrix identity_matrix(int n) {
+        vector< vector< long double> > values(n, vector<long double>(n, 0.0));
+        for (int i = 0; i < n; i++)
+            values[i][i] = 1.0;
+        return values;
+    }
+
+    Matrix operator*(const Matrix &other) const {
+        int n = n_rows, m = other.n_cols;
+        vector< vector<long double> > result(n_rows, vector<long double>(n_cols, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                long double tmp = 0;
+                for (int k = 0; k < n_cols; k++) {
+                    tmp += mat[i][k] * 1ll * other.mat[k][j];
+                    while (tmp >= MOD2)
+                        tmp -= MOD2;
+                }
+                result[i][j] = tmp;
+            }
+
+        return move(Matrix(move(result)));
+    }
+
+    inline bool is_square() const {
+        return n_rows == n_cols;
+    }
+};
+
+// binary exponentiation, returns a^p
+Matrix pw(Matrix a, int p) {
+    Matrix result = Matrix::identity_matrix(a.n_cols);
+    while (p > 0) {
+        if (p & 1)
+            result = a * result;
+        a = a * a;
+        p >>= 1;
+    }
+    return result;
+}
+
