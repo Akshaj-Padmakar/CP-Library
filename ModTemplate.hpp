3659ecb029ca877c830f5ed5aef3ae36db8067f0
Akshaj <67319076+Akshaj-Padmakar@users.noreply.github.com>
2022-06-27 10:13:16 +0530

    Create ModTemplate.hpp

---
+++ ModTemplate.hpp
@@ -0,0 +1,19 @@
+const int MOD = 998244353;
+
+template<typename T> T gcd(T a, T b) {if (b == 0) return a; a %= b; return gcd(b, a);}
+template<typename T> T lcm(T a, T b) {return (a * (b / gcd(a, b)));}
+
+void checker(int &res) {res %= MOD; res += MOD; res %= MOD;}
+int add(int a, int b, int c = MOD) {int res = a + b; checker(res); return res;}
+int sub(int a, int b, int c = MOD) {int res = a - b; checker(res); return res;}
+int mul(int a, int b, int c = MOD) {int res = (long long) a * b; checker(res); return res;}
+
+template<typename T>T binpow(T e, T n) {T x = 1, p = e; while (n) {if (n & 1)x = x * p; p = p * p; n >>= 1;} return x;}
+template<typename T>T binpow2(T e, T n, T m = MOD) {T x = 1, p = e; while (n) {if (n & 1)x = mul(x, p, m); p = mul(p, p, m); n >>= 1;} return x;}
+template<typename T>T extended_euclid(T a, T b, T &x, T &y) {
+    T xx = 0, yy = 1; y = 0; x = 1; while (b) {
+        T q = a / b, t = b; b = a % b; a = t; \
+        t = xx; xx = x - q * xx; x = t; t = yy; yy = y - q * yy; y = t;
+    } return a;
+}
+template<typename T>T mod_inverse(T a, T n = MOD) {T x, y, z = 0; T d = extended_euclid(a, n, x, y); return (d > 1 ? -1 : sub(x, z, n));}
