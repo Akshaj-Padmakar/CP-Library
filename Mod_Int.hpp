977580d64fbe861b38702b2602e0b23fd17d0a2f
Akshaj <67319076+Akshaj-Padmakar@users.noreply.github.com>
2022-06-27 10:02:14 +0530

    Create Mod_Int.hpp

---
+++ Mod_Int.hpp
@@ -0,0 +1,94 @@
+/*
+Instructions - normal_mod > mod_int > mod_template(add,sub)
+*/
+
+const int MOD = 998244353;
+
+template< int mod >struct ModInt {
+public:
+	ModInt() : x(0) {}
+
+	ModInt(int _x) {
+		_x %= mod; _x += mod; _x %= mod;
+		x = _x;
+	}
+
+	ModInt &operator+=(const ModInt &_x) {
+		x += _x.x;
+		if (x >= mod) {
+			x -= mod;
+		}
+		return *this;
+	}
+
+	ModInt &operator-=(const ModInt &_x) {
+		x -= _x.x;
+		if (x < 0) {
+			x += mod;
+		}
+		return *this;
+	}
+
+	ModInt &operator*=(const ModInt &_x) {
+		x *= _x.x; x %= mod;
+
+		return *this;
+	}
+
+	ModInt &operator/=(const ModInt &_x) {
+		*this *= _x.inverse();
+		return *this;
+	}
+
+	ModInt operator-() const {return ModInt(-x); }
+
+	ModInt operator+(const ModInt &_x) const { return ModInt(*this) += _x; }
+
+	ModInt operator-(const ModInt &_x) const { return ModInt(*this) -= _x; }
+
+	ModInt operator*(const ModInt &_x) const { return ModInt(*this) *= _x; }
+
+	ModInt operator/(const ModInt &_x) const { return ModInt(*this) /= _x; }
+
+	bool operator==(const ModInt &_x) const { return x == _x.x; }
+
+	bool operator!=(const ModInt &_x) const { return x != _x.x; }
+
+	ModInt inverse() const {
+		int a = x, b = mod, u = 1, v = 0, t;
+		while (b > 0) {
+			t = a / b;
+			swap(a -= t * b, b);
+			swap(u -= t * v, v);
+		}
+		return ModInt(u);
+	}
+
+	ModInt binpow(int n) const {
+		ModInt ans(1), mul(x);
+		while (n > 0) {
+			if (n & 1) {
+				ans *= mul;
+			}
+			mul *= mul;
+			n >>= 1;
+		}
+		return ans;
+	}
+
+	friend ostream &operator<<(ostream &os, const ModInt &p) {
+		return os << p.x;
+	}
+
+	friend istream &operator>>(istream &is, ModInt &a) {
+		int t;
+		is >> t;
+		a = ModInt< mod >(t);
+		return (is);
+	}
+
+private:
+	int x;
+};
+
+using mint = ModInt< MOD >;
